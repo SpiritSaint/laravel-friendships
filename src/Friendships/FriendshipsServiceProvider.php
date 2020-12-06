528902a2f1c22163d6915f38d7faef8c62a53c0b
Ian Torres <iantorres@outlook.com>
2020-12-05 23:49:40 -0300

    Code fixed and service provider added.

---
+++ src/Friendships/FriendshipsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SpiritSaint\Friendships;
+
+use Illuminate\Support\ServiceProvider;
+
+class FriendshipsServiceProvider extends ServiceProvider {
+    /**
+     * Bootstrap any application services.
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
